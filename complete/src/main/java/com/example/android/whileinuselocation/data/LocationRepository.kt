a79041344025d2c0e53074071d4f192c35c67fae
Sean Barbeau <sjbarbeau@gmail.com>
2021-05-05 17:59:43 -0400

    chore: Remove unnecessary suspend modifier

--- complete/src/main/java/com/example/android/whileinuselocation/data/LocationRepository.kt
+++ complete/src/main/java/com/example/android/whileinuselocation/data/LocationRepository.kt
@@ -9,12 +9,7 @@ class LocationRepository @Inject constructor(
         private val locationDao: LocationDao
     ) {
 
-    // By default Room runs suspend queries off the main thread, therefore, we don't need to
-    // implement anything else to ensure we're not doing long running database work
-    // off the main thread.
-    @Suppress("RedundantSuspendModifier")
-    @WorkerThread
-    suspend fun getLocations() = locationDao.getLocations()
+    fun getLocations() = locationDao.getLocations()
 
     // By default Room runs suspend queries off the main thread, therefore, we don't need to
     // implement anything else to ensure we're not doing long running database work
